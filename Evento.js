1e197a924d827f83cb19538ec8700e9d38bb4337
AndreDantasss <andre.viictor@gmail.com>
2021-12-07 23:49:32 -0300

    adicionando evento

---
+++ Evento.js
@@ -0,0 +1,25 @@
+// O evento só poderá ser marcado entre os dias 7 e 28
+
+let DataEvento = 7
+
+if (DataEvento >= 7) {
+    console. log("Data disponivel")
+} else {
+    console. log("Data indisponivel")
+}
+
+let IdadeParticipante = 18
+
+if (IdadeParticipante >= 18){
+    console. log("O participante pode se cadastrar")
+} else {
+    console. log("O participante precisa ter no minimo 18 anos")
+}
+
+let ListaParticipantes = 100
+
+if (ListaParticipantes <= 100){
+    console. log("O evento pode ser registrado")
+} else {
+    console. log("O evento permite no máximo 100 participantes")
+}
